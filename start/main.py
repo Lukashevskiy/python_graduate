058ca03f11ccab8ab03577ef5d58be9ba67173bd
Dmitriy <Dim4ik-super-klass@mail.ru>
2022-11-22 21:42:58 +0800

    add circle

--- start/main.py
+++ start/main.py
@@ -1,10 +1,14 @@
 import pygame as pg
+from random import randint
 
 WINDOW_WIDTH, WINDOW_HEIGTH = 1280, 720
 FPS = 60
 
 
 pg.init()
+
+x, y, r = randint(100, 1600), randint(100, 620), randint(10, 100)
+color = (randint(100, 255), 0, 0)
 
 screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGTH))
 clock = pg.time.Clock()
@@ -14,12 +18,15 @@ while is_running:
     clock.tick(FPS)
 
     # обработка событий
-
+    for event in pg.event.get():
+        if event.type == pg.QUIT:
+            is_running = False
+            break
 
     # обработка состояния игры
 
 
     # отрисовка
-    
-
+    screen.fill((0, 155, 0))
+    pg.draw.circle(screen, color, (x, y), r)
     pg.display.flip()

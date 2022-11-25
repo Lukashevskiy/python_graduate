1877393cfd6805a2d1bbd3c9504b5ac65329b59b
Dmitriy <Dim4ik-super-klass@mail.ru>
2022-11-25 21:55:08 +0800

    add moveing

--- dz_circles/main.py
+++ dz_circles/main.py
@@ -1,23 +1,47 @@
 import pygame as pg
 from random import randint
 
+
+
 WINDOW_WIDTH, WINDOW_HEIGTH = 1280, 720
 FPS = 60
+
+class Circle:
+
+    def __init__(self):
+        self.x = randint(100, 1180)
+        self.y = randint(100, 610)
+
+        self.dx = randint(-3, 3)
+        self.dy = randint(-3, 3)
+        
+        self.radius = randint(10, 20)
+        
+        self.color = (randint(100, 255), 0, 0) 
+
+    def update(self):
+        self.x = (self.x + self.dx)
+        self.y = (self.y + self.dy)
+
+
+    def check_collision(self, other):
+        if self.x + self.radius >= other.right or self.x - self.radius <= other.left:
+            self.dx *= -1
+        if self.y + self.radius >= other.bottom or self.y - self.radius <= other.top:
+            self.dy *= -1 
+
+    def draw(self, screen):
+        pg.draw.circle(screen, self.color, (self.x, self.y), self.radius)
+
 
 
 pg.init()
 
-def update_position():
-    return randint(100, 1180), randint(100, 620)
-    
-xs = [randint(100, 1180) for i in range(100)] 
-ys = [randint(100, 620) for i in range(100)] 
-rs = [randint(10, 100) for i in range(100)]
-
-colors = [(randint(100, 255), 0, 0) for i in range(100)]
 
 screen = pg.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGTH))
 clock = pg.time.Clock()
+
+circles = [Circle() for _ in range(100)]
 
 is_running = True
 
@@ -32,17 +56,16 @@ while is_running:
             is_running = False
             break
         elif event.type == pg.KEYDOWN:
-            if event.key == pg.K_s:
-                is_circle_change = True
+            if event.key == pg.K_q:
+                is_running = False
 
     # обработка состояния игры
-    if is_circle_change:
-        for i in range(100):
-            xs[i], ys[i] = update_position()
-        is_circle_change = False
-
+    for circle in circles:
+        circle.update()
+        circle.check_collision(screen.get_rect())
+        
     # отрисовка
     screen.fill((0, 155, 0))
-    for x, y, r, color in zip(xs, ys, rs, colors):
-        pg.draw.circle(screen, color, (x, y), r)
+    for circle in circles:
+        circle.draw(screen)
     pg.display.flip()
